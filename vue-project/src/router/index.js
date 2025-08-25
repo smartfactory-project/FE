d37ef73eec19d5fcf7cfb6656779af7dc571c196
Hadoyi <hadoe99@naver.com>
2025-08-25 15:12:35 +0900

    STYLE : 소스코드 경로 변경

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -4,9 +4,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   { path: '/', name: 'home', component: () => import('@/views/HomePage.vue') },
   { path: '/register', name: 'register', component: () => import('@/pages/RegisterForm.vue') }, 
-  { path: '/announcements', name: 'announcements', component: () => import('@/pages/Announcements.vue') }, 
-  { path: '/production', name: 'production', component: () => import('@/pages/Production.vue') }, 
-  { path: '/charts', name: 'charts', component: () => import('@/pages/Charts.vue') }, 
+  { path: '/announcements', name: 'announcements', component: () => import('@/views/Announcements.vue') }, 
+  { path: '/production', name: 'production', component: () => import('@/views/Production.vue') }, 
+  { path: '/charts', name: 'charts', component: () => import('@/views/Charts.vue') }, 
   { path: '/posts', name: 'posts', component: () => import('@/views/PostsPage.vue') },
   { path: '/schedule', name: 'schedule', component: () => import('@/views/SchedulePage.vue') },
   { path: '/announcements', name: 'announcements', component: () => import('@/views/AnnouncementsPage.vue') }
